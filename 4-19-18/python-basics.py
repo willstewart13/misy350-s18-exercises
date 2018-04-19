34e519aff1a2376a0c87446ad2f3da7e5b434b7e
willstewart13 <wpstewart13@gmail.com>
2018-04-19 10:23:52 -0400

    add exercises

---
+++ 4-19-18/python-basics.py
@@ -0,0 +1,9 @@
+# this is my first python code
+greeting = "Hello"
+name = "Will"
+print greeting
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting + " " + name + "!"
+print "%s %s!" % (greeting, name)
